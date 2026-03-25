941243891f4da70090e27afaba76c630a53545a3
wtl985 <1364600104@qq.com>
2026-03-25 20:46:53 +0800

    Update train.py

--- train.py
+++ train.py
@@ -1,17 +1,17 @@
 # -*- coding: utf-8 -*-
 import warnings
 warnings.filterwarnings('ignore')
-from SDSHNet import RTDETR
+from LDFFNet import RTDETR
 
 
 if __name__ == '__main__':
 
     model = RTDETR(model=r'/LDFFNet/cfg/models/LDFFNet.yaml')
-    #model.load('SDSHNet.pt')
+    #model.load('LDFFNet.pt')
     model.train(data=r'/LDFFNet/jiazawu.yaml',
                 imgsz=640,
-                epochs=1,
-                batch=4,
+                epochs=300,
+                batch=16,
                 project='runs/train',
                 name='exp',
                 workers=0,
